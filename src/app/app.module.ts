469ad2d45dc4a9665bdd93458dcd1c0540ecad3a
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-01-03 05:32:33 +0100

    mainsection added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainsectionComponent } from './mainsection/mainsection.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainsectionComponent,
+    SkillsSectionComponent
   ],
   imports: [
     BrowserModule,
