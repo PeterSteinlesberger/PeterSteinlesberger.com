7c1353747055de5c3467a380fe042703aa3e8467
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-02-25 15:15:02 +0100

    fix: onScrollFunction

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { InViewPortService } from '../in-view-port.service';
+// import { Router } from '@angular/router';
+// import { InViewPortService } from '../in-view-port.service';
 import { NavigationService } from '../navigation.service';
 @Component({
   selector: 'app-navbar',
@@ -11,15 +11,15 @@ export class NavbarComponent implements OnInit {
   changeImg = false;
 
   constructor(
-    public navigation: NavigationService,
-    public inviewport: InViewPortService
+    public navigation: NavigationService
+   // public inviewport: InViewPortService
   ) {}
 
   navOpened = false;
 
   ngOnInit(): void {}
   
- toggleNav(event) {
+ toggleNav(event : any) {
       console.log(event);
       this.navOpened = !this.navOpened;
     }
---
+++ src/app/navigation.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class NavigationService {
+  constructor(public router: Router) {}
+
+  forceNavigate(name: string) {
+    this.router
+      .navigate(['/'], { fragment: name })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  scroll() {
+    let navbar = document.getElementById('navbar') as HTMLElement;
+    if (this.router.url !== '/imprint') {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.style.top = '0';
+      } else {
+        navbar.style.top = window.innerHeight - window.scrollY + 'px';
+      }
+    }
+  }
+
+  _visibilityChangeHandler(event: any) {
+    console.log(event);
+  }
+}
