421c1a2442bc0ba6ccd612f07f26bc49be06a5c5
PeterSteinlesberger <petersteinlesberger@mail.de>
2022-03-04 00:50:52 +0100

    optimize: footer icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+changeImg = false;
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { Router } from '@angular/router';
  import { InViewPortService } from '../in-view-port.service';
 import { NavigationService } from '../navigation.service';
 @Component({
